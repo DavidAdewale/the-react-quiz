0ad164cfa684d35fb23d851a052a62418048dc96
unknown <adewaledavidola@gmail.com>
2023-07-04 14:35:58 +0100

    initialized state and built the intro section of the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import DateCounter from './DateCounter';
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import { useEffect, useReducer } from 'react';
+
+const initalState = {
+  questions: [],
+  //Status states: 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataRecieved':
+      return { ...state, questions: action.payload, status: 'ready' };
+
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+
+    default:
+      throw new Error('Action unknown');
+  }
+}
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initalState);
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch('http://localhost:9000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataRecieved', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+      </Main>
     </div>
   );
 }
